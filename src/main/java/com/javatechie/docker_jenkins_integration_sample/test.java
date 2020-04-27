5dfdf74e0f6d002ab85f89db573aee7f0ece569c
smita1994 <smitasalunke13@gmail.com>
2020-04-27 18:35:51 +0530

    second commit

---
+++ src/main/java/com/javatechie/docker_jenkins_integration_sample/test.java
@@ -0,0 +1,11 @@
+package com.javatechie.docker_jenkins_integration_sample;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("welcome to devops operation ");
+	}
+
+}
